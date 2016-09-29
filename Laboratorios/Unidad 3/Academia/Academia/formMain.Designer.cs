c05decce461b798001e0e38ced3a7642d2c422b2
Martín <guereta.martin@gmail.com>
2016-09-29 13:23:53 -0300

    add U3 LAB 1 (Login)

---
+++ Laboratorios/Unidad 3/Academia/Academia/formMain.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Escritorio
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnsPrinicpal = new System.Windows.Forms.MenuStrip();
+            this.mnuArchivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSalir = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsPrinicpal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnsPrinicpal
+            // 
+            this.mnsPrinicpal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuArchivo});
+            this.mnsPrinicpal.Location = new System.Drawing.Point(0, 0);
+            this.mnsPrinicpal.Name = "mnsPrinicpal";
+            this.mnsPrinicpal.Size = new System.Drawing.Size(284, 24);
+            this.mnsPrinicpal.TabIndex = 1;
+            this.mnsPrinicpal.Text = "menuStrip1";
+            // 
+            // mnuArchivo
+            // 
+            this.mnuArchivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSalir});
+            this.mnuArchivo.Name = "mnuArchivo";
+            this.mnuArchivo.Size = new System.Drawing.Size(60, 20);
+            this.mnuArchivo.Text = "Archivo";
+            // 
+            // mnuSalir
+            // 
+            this.mnuSalir.Name = "mnuSalir";
+            this.mnuSalir.Size = new System.Drawing.Size(152, 22);
+            this.mnuSalir.Text = "Salir";
+            this.mnuSalir.Click += new System.EventHandler(this.mnuSalir_Click);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.mnsPrinicpal);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.mnsPrinicpal;
+            this.Name = "formMain";
+            this.Text = "Academia";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Shown += new System.EventHandler(this.formMain_Shown);
+            this.mnsPrinicpal.ResumeLayout(false);
+            this.mnsPrinicpal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnsPrinicpal;
+        private System.Windows.Forms.ToolStripMenuItem mnuArchivo;
+        private System.Windows.Forms.ToolStripMenuItem mnuSalir;
+    }
+}
---
+++ Laboratorios/Unidad 3/Academia/Negocio/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Entidades;
+
+namespace Negocio
+{
+    public class UserController
+    {
+        public Usuario getUsuario() { 
+        UsuarioData ud = new UsuarioData();
+        Usuario usu = ud.getUsuario();
+            return usu;
+        }
+    }
+}
